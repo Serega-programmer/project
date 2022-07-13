eba1c31d81a4a83ae770ecaf8e01d9207da43e1b
Serega <investor104511@gmail.com>
2022-07-14 01:21:19 +0300

    14.07

--- script.js
+++ script.js
@@ -88,3 +88,38 @@ let incr = 10,
         //  Пример: в браузере - результат Привет, Serega. Т.о. можно подставлять значения из любой БД.
         const user = "Serega";
         alert(`Привет, ${user}`);
+
+        // п.20 УСЛОВИЯ
+         if(4==4) {
+            console.log(`Ok!`);
+         }
+         if(4==5) {
+            console.log(`OK`);
+         } else {
+            console.log(`Error`);
+         }
+        
+        //  Создадим переменную num=50
+        // const num=50
+        // if(num<49){
+        // console.log(`Error`);
+        // } else if(num>100) {
+        //     console.log(`Много`);
+        // } else {
+        //     console.log(`Ok`);
+        // }
+        //     (num===50) ? console.log(`Ok`) : console.log(`Error`);
+
+            const num = 50;
+         switch (num) {
+            case 49:
+                console.log(`Неверно`);
+                break;
+                case 100:
+                    console.log(`Неверно`);
+                    break;
+                    case 50:
+                    console.log(`В точку!`);
+                    default:
+                        console.log(`Не в этот раз`);
+                    }
