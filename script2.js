4658adbe3b31b4f20d6c7b4a32bc46ea493ca399
investor104511@gmail.com <investor104511@gmail.com>
2022-07-22 08:16:47 +0300

    22.07

--- script2.js
+++ script2.js
@@ -1,5 +1,25 @@
 "use strict";
-function sayHello(name) {
-    return "Привет, ${name}";
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
 }
-sayHello ("Alex");
+
+console.log(arrayOfNumbers);
+
+const str = "TEST";
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("it"));
+
+const logg = "Hello world";
+console.log(logg.substr(6,5));
+
+const test = "12.2px";
+console.log(parseFloat(test));
+
+const answer = 10;
+console.log(typeof(answer));
