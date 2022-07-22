2505f05214bbe1828b678b83d111b3108f1cf8bd
Serega-programmer <108458786+Serega-programmer@users.noreply.github.com>
2022-07-22 23:17:02 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,4 @@
 "use strict";
-<<<<<<< HEAD
 
 const arrayOfNumbers = [];
 
@@ -24,15 +23,13 @@ console.log(parseFloat(test));
 
 const answer = 10;
 console.log(typeof(answer));
-=======
-<<<<<<< HEAD
+
 
    const arr = [1,3,4,56,7,8];
    console.log (arr.length);
-=======
+
 function sayHello(name) {
     return "Привет, ${name}";
 }
 sayHello ("Alex");
->>>>>>> dcb9ae3c50f421b78d3a9aef5fe64b02dd6c5d8a
->>>>>>> e8c3ba1440574b0a813d5110e59bd4de71179433
+
