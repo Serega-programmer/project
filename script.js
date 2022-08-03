14183a79e9f0cce1591ab875454ff52e6e7e364d
investor104511@gmail.com <investor104511@gmail.com>
2022-08-03 15:17:14 +0300

    03.08

--- script.js
+++ script.js
@@ -386,3 +386,33 @@ let answ = +prompt("Hello world", "");
 // 0, "", null, undefined, NaN - false
 
 let switcher = null;
+
+// let number = 5; debugger
+    // function lognumber () {
+
+    //     console.log (number); debugger
+
+    // }
+    // number = 6;
+    // lognumber(); debugger
+
+    // number = 8;
+    // lognumber ();
+
+    function createCounter() {
+        let counter = 0;
+
+        const myFunction = function () { debugger
+            counter = counter + 1; debugger
+            return counter; debugger
+        };
+
+        return myFunction;
+    }
+debugger
+    const increment = createCounter();
+    const c1 = increment(); debugger
+    const c2 = increment();debugger
+    const c3 = increment();debugger
+
+    console.log(c1, c2, c3); 
