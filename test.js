b23e4df55cf7f988c3fa1b119a143a766ace28f4
investor104511@gmail.com <investor104511@gmail.com>
2022-08-03 15:03:45 +0300

    test

--- test.js
+++ test.js
@@ -2,40 +2,40 @@
 
 
 
-const obj = {
-    name: `John`,
-    age: 25,
-    isMarried: false
-};
-console.log(obj);
+// const obj = {
+//     name: `John`,
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj);
 
-const arr = [1, 2, {}, []];
-console.log(arr[2]);
+// const arr = [1, 2, {}, []];
+// console.log(arr[2]);
 
-const storeName = `Belka`;
-const storeDescription = {
-    budget: 10000,
-    employes: [`Alex`, `Adam`, `Nik`],
-    progucts: {
-        "butter": 100,
-        "bread": 50
-    },
-    open: true
-};
-console.log(storeDescription);
-console.log(storeName);
+// const storeName = `Belka`;
+// const storeDescription = {
+//     budget: 10000,
+//     employes: [`Alex`, `Adam`, `Nik`],
+//     progucts: {
+//         "butter": 100,
+//         "bread": 50
+//     },
+//     open: true
+// };
+// console.log(storeDescription);
+// console.log(storeName);
 
-const result = confirm(`Are you here?`);
-console.log(result);
+// const result = confirm(`Are you here?`);
+// console.log(result);
 
-const answer = +prompt(`Вам есть 18?`, `18`);
-console.log(answer + 5);
+// const answer = +prompt(`Вам есть 18?`, `18`);
+// console.log(answer + 5);
 
-if (4==4) {
-    console.log(`OK!`);
-    } else {
-        console.log(`Error`);
-    }
+// if (4==4) {
+//     console.log(`OK!`);
+//     } else {
+//         console.log(`Error`);
+//     }
 
 //     let num  = 50;
 //     if(num < 49) {
@@ -49,13 +49,27 @@ if (4==4) {
 
 //     }
 
-    const num = 50;
-    switch (num) {
-        case 49:
-            console.log(`Не верно`);
-            break;
-            case 100:
-                console.log(`Не верно`);
-                case 50:
-                    console.log(`В точку`);
+    // const num = 50;
+    // switch (num) {
+    //     case 49:
+    //         console.log(`Не верно`);
+    //         break;
+    //         case 100:
+    //             console.log(`Не верно`);
+    //             case 50:
+    //                 console.log(`В точку`);
+    // }
+
+    let result = "";
+    const length = 7;
+
+    for (let i = 1; i < length; i++) {
+
+        for ( let j = 0; j < i; j++) {
+            result += "*";
+        }
+        result += "\n";
     }
+
+    
+    console.log(result);
